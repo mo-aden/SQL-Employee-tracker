5e9474583f248270c1a73b3295a471bca0bddeac
mo-aden <mohamedaden17@outlook.com>
2022-12-08 13:54:25 -0700

    added init() func

--- server.js
+++ server.js
@@ -3,24 +3,29 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "userOption",
-      message: "Select an action from the options below (use arrow keys)",
-      choices: [
-        "View All Employees",
-        "Add an Employee",
-        "Update an Employee Role",
-        "View All Roles",
-        "Add Role",
-        "View All Departments",
-        "Add Department",
-        "Exit",
-      ],
-    },
-  ])
-  .then((response) => {
-    console.log(response);
-  });
+const init = function () {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "userOption",
+        message: "Select an action from the options below (use arrow keys)",
+        choices: [
+          "View All Employees",
+          "Add an Employee",
+          "Update an Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+          "Exit",
+        ],
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+    });
+};
+
+//initialize the starter questions
+init();
