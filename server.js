712eb716f71926fc4a5ab1e84a2c643fcfeb5e95
mo-aden <mohamedaden17@outlook.com>
2022-12-08 13:37:28 -0700

    added the prompt questions

---
+++ server.js
@@ -0,0 +1,26 @@
+//Imports
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "userOption",
+      message: "Select an action from the options below (use arrow keys)",
+      choices: [
+        "View All Employees",
+        "Add an Employee",
+        "Update an Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Exit",
+      ],
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+  });
