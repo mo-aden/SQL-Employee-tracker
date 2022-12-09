409dee58efb7f7e34cc6dcbacb4e036293a14f6d
mo-aden <mohamedaden17@outlook.com>
2022-12-08 19:31:38 -0700

    completed the func of the dept table

--- index.js
+++ index.js
@@ -26,11 +26,51 @@ const init = function () {
     ])
     .then(({ userOption }) => {
       console.log(userOption);
-
+      userOption === "View All Departments" && viewAllDepartments();
+      userOption === "Add Department" && addNewDepartment();
       userOption === "View All Employees" && viewAllEmployees();
       userOption === "Exit the application" && Exit();
     });
 };
+
+//Display all the dept already created
+function viewAllDepartments() {
+  const query = `SELECT * FROM department`;
+  db.query(query, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    console.table(results);
+    init();
+  });
+}
+
+//adds new dept to the db
+function addNewDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "newDepartment",
+        message: "What department would you like to add?",
+      },
+    ])
+    .then(({ newDepartment }) => {
+      console.log(newDepartment);
+
+      const query = `INSERT INTO department(name) VALUES ("${newDepartment}")`;
+
+      db.query(query, (err, results) => {
+        if (err) {
+          throw err;
+        }
+        console.table(results);
+        init();
+      });
+    });
+}
+
+//Displays all the roles
 
 function viewAllEmployees() {
   const query = `SELECT * FROM employee`;
